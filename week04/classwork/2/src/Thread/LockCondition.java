7ef797384cefc1df93a0a60b71bba7596b064913
xyz2b <jiaohongyua@gmail.com>
2021-04-13 10:10:08 +0800

    week04

---
+++ week04/classwork/2/src/Thread/LockCondition.java
@@ -0,0 +1,58 @@
+package Thread;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Task3 implements Runnable {
+    public int sum;
+    private final Condition signal;
+    private final Lock lock;
+
+    public Task3(Condition signal, Lock lock) {
+        this.signal = signal;
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        lock.lock();
+
+        try {
+            this.sum = sum();
+            signal.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private int sum() {
+        return fibo(36);
+    }
+
+    private int fibo(int a) {
+        if ( a < 2)
+            return 1;
+        return fibo(a-1) + fibo(a-2);
+    }
+}
+
+public class LockCondition {
+    final static Lock lock = new ReentrantLock();
+    final static Condition signal  = lock.newCondition();
+
+    public static void main(String[] args) throws InterruptedException {
+        lock.lock();
+        try {
+            Task3 task = new Task3(signal, lock);
+
+            new Thread(task, "task").start();
+
+            signal.await();
+
+            System.out.println("异步计算结果为：" + task.sum);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
--- week04/classwork/2/src/Thread/SingleThreadExecutor.java
+++ week04/classwork/2/src/Thread/SingleThreadExecutor.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Future;
 public class SingleThreadExecutor {
     public static void main(String[] args) throws Exception {
         // 实现Callable接口的实例可以直接丢给线程池submit，并且可以获取返回值(Callable::call方法)，但不能直接丢给Thread::start执行
-        // 使用FutureTask可以把Callable封装成一个带返回值的任务，FutureTask可以直接丢给Thread::start执行
+        // 使用FutureTask可以把Callable封装成一个带返回值的任务，FutureTask可以直接丢给Thread::start执行，也可以直接丢给线程池submit执行
 
         // 实现Runnable接口的实例可以直接丢给Thread::start执行，也可以直接丢给线程池submit，但不能获取返回值(Runnable::run方法)
         Callable<Integer> task = () -> {
